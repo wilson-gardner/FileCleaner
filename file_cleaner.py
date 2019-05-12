112dc1d186a36ee1e811f1e8507517323d05a8db
Wilson Gardner <wilsonalangardner@gmail.com>
2019-05-12 01:00:18 -0400

    Update file_cleaner.py
    Added in additional imports. Created toast notification that will inform user how many total files were deleted. Expanded the deletion of files to multiple directories (specified by user). Added status bar and other status text for console. General refactoring.

--- file_cleaner.py
+++ file_cleaner.py
@@ -1,19 +1,53 @@
 import time
-from os import listdir, remove
+from os import listdir, remove, system, name
 from os.path import getmtime
+from win10toast import ToastNotifier
+from tqdm import tqdm
 
-# get current time
-curr_time = time.time()
-# currently hard coded for 1 week
-max_age = 604800 
-# path to folder to clean
-path = "/path/to/folder/"
 
-#loop through all folder objects (both files and folders)
-for obj in listdir(path):
-    # full file path for getmtime()
-    file_path = "{}/{}".format(path, obj)
-    # check if modified over 1 week ago
-    if (curr_time - getmtime(file_path)) > max_age:
-        # delete file
-        remove(file_path)
+#VARIABLES
+notifier = ToastNotifier()
+status = tqdm()
+max_age = 604800
+folders = [ "D:\\Downloads" ]
+total_files_cleaned = 0
+
+
+#FUNCTIONS
+def clear_command_line():
+    if name == 'nt':
+        system('cls')
+    else:
+        system('clear')
+
+
+def attempt_clear_files(folder):
+    print("Attempting to clear {}:".format(folder))
+    del_obj = []
+    objects = listdir(folder)
+    status.total = len(objects)
+    for obj in objects:
+        if (time.time() - getmtime(get_file_path(folder, obj))) > max_age:
+            del_obj.append(obj)
+    if len(del_obj) > 0:
+        for obj in del_obj:
+            remove(get_file_path(folder, obj))
+            total_files_cleaned = total_files_cleaned + 1
+            status.update()
+        status.close()
+        print("-> File cleanup complete for {}...".format(folder))
+    else:
+        print("-> No files to clear.")
+        print("Cancelling.........")
+
+
+def get_file_path(folder, obj):
+    return "{}/{}".format(folder, obj)
+
+
+# MAIN PROGRAM
+clear_command_line()
+print("Beginning file cleanup...")
+for folder in folders:
+    attempt_clear_files(folder)
+notifier.show_toast("File Cleaner", "{} files cleaned.".format(total_files_cleaned))
