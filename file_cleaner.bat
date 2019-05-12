e98973087418d5dfe5eed7b78e417398beaae3dd
Wilson Gardner <wilsonalangardner@gmail.com>
2019-05-12 01:07:15 -0400

    Update file_cleaner.bat
    Added comments to batch file

--- file_cleaner.bat
+++ file_cleaner.bat
@@ -1,1 +1,4 @@
+rem Batch file allows system Python dist to run file_cleaner.py
+rem Can be used with tasks or as startup script
+rem Format: "either python command or path to python.exe" "path to file_cleaner.py"
 python3 D:\\Github\\FileCleaner\\file_cleaner.py
